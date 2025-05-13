3c1a9067e3a78b0f260636c06012ff7c51a6e30d
Valerio Cassone <valerio.cassone@studenti.unitn.it>
2025-05-13 17:26:18 +0200

    Implementazione DB: DatabaseClient.js

---
+++ Backend/src/database/DatabaseClient.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+class DatabaseClient {
+    constructor() {
+        this._status = false;
+    }
+
+    async connect() {
+        if (this._status) {
+            return this._connection;
+        }
+
+        const uri = process.env.DB_URI;
+        if (!uri) {
+            throw new Error('La variabile d\'ambiente DB_URI non è definita');
+        }
+
+        try {
+            await mongoose.connect(process.env.DB_URI, {
+                dbName: process.env.DB_NAME,
+            });
+            this._status = true;
+        } catch (error) {
+            console.error('[MongoDB] Errore durante la connessione: ', error);
+            throw error;
+        }
+
+        mongoose.connection.on('connected', () => { console.log('[MongoDB] Connesso'); });
+        mongoose.connection.on('error', (err) => { console.error('[MongoDB] Errore durante la connessione: ', err); });
+        mongoose.connection.on('disconnected', () => { console.log('[MongoDB] Disconnesso'); });
+    }
+
+    get status() {
+        return this._status;
+    }
+
+    async disconnect() {
+        await mongoose.connection.close();
+    }
+}
+
+export default new DatabaseClient();
+export { DatabaseClient };
--- Backend/src/database/connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-console.log(process.env.DB_URL);
-
-class Database {
-    connect() {
-        mongoose.connect(process.env.DB_URL);
-    }
-}
