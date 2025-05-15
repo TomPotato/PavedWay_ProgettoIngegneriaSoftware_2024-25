5b061b8e67dbe44f2ca41c158f28f95068c4a1ab
Valerio Cassone <valerio.cassone@studenti.unitn.it>
2025-05-15 11:55:34 +0200

    Strutturazione file del progetto

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const eventRoutes = require('./eventRoutes');
+const userRoutes = require('./userRoutes');
+
+router.use('/events', eventRoutes);
+router.use('/users', userRoutes);
+
 module.exports = router;
