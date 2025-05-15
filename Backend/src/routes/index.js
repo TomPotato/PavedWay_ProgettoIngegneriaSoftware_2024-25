77ec0f5cae5751dfbda29de9a251f02796bd2724
Nicola Panozzo <nicola.panozzo17@gmail.com>
2025-05-15 20:22:31 +0200

    Last changes to route and siteRoute for site by Panozzo

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const eventRoutes = require('./eventRoutes');
 const userRoutes = require('./userRoutes');
+const siteRoutes = require('./siteRoutes');
 
 router.use('/events', eventRoutes);
 router.use('/users', userRoutes);
+router.use('/site', siteRoutes);
 
 module.exports = router;
