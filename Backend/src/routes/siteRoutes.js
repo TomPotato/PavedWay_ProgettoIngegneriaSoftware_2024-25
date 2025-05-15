a4be9e98a198fc9518763760cda7f857ae495e08
Nicola Panozzo <nicola.panozzo17@gmail.com>
2025-05-15 20:01:40 +0200

    SiteRoutes.js creation by Panozzo

---
+++ Backend/src/routes/siteRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const { SiteService } = require('../services/SiteService');
+
+router.get('/', async (req, res) => {
+    try {
+        res.status(200).json(sites);
+    } catch (error) {
+        console.error('Error fetching reports:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+module.exports = router;
