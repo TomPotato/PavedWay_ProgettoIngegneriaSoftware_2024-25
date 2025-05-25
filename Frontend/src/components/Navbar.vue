6d9b3bf7910acaa00dda433cfaa7ae4f864f54f9
Valerio Cassone <valerio.cassone@studenti.unitn.it>
2025-05-25 17:39:15 +0200

    Implementazione Logout e Reindirizzamento

--- Frontend/src/components/Navbar.vue
+++ Frontend/src/components/Navbar.vue
@@ -35,7 +35,7 @@
                                 <RouterLink to="/register">Registrati</RouterLink>
                             </li>
                             <li v-else>
-                                <RouterLink to="/logout">Disconnettiti</RouterLink>
+                                <span @click="openModal">Disconnettiti</span>
                             </li>
                         </ul>
                     </div>
@@ -43,12 +43,49 @@
             </ul>
         </div>
     </div>
+
+    <dialog id="logout" class="modal">
+        <div class="modal-box max-w-md w-full">
+            <h3 class="text-lg font-bold">Disconnessione</h3>
+            <p class="py-2">Sei sicuro di volerti disconnettere?</p>
+            <div class="modal-action">
+                <form method="dialog" class="flex flex-row gap-2">
+                    <button class="btn btn-primary" @click="logout">Disconnettiti</button>
+                    <button class="btn btn-primary" @click="closeModal">Annulla</button>
+                </form>
+            </div>
+        </div>
+        <form method="dialog" class="modal-backdrop">
+            <button @click="closeModal">Annulla</button>
+        </form>
+    </dialog>
 </template>
 
 <script setup>
+import { useRouter, useRoute } from 'vue-router';
 import { useAuthStore } from '@/stores/authStores';
+import { nextTick } from 'vue';
 
+const router = useRouter();
+const route = useRoute();
 const store = useAuthStore();
+
+const openModal = () => {
+    document.getElementById('logout').showModal();
+    document.body.classList.add('modal-open');
+};
+
+const closeModal = () => {
+    document.getElementById('logout').close();
+    document.body.classList.remove('modal-open');
+};
+
+const logout = () => {
+    store.logout();
+    store.setMessage('Logout effettuato con successo!');
+    router.replace({ path: '/refresh', query: { redirect: route.fullPath } });
+    closeModal();
+};
 
 defineOptions({ name: 'Navbar' });
 </script>
--- Frontend/src/routes/index.js
+++ Frontend/src/routes/index.js
@@ -6,13 +6,15 @@ import Home from '../pages/Home.vue';
 import Login from '../pages/Login.vue';
 import Register from '../pages/Register.vue';
 import Events from '../pages/Events.vue';
+import Refresh from '../pages/Refresh.vue';
 
 const routes = [
     { path: '/', name: 'home', component: Home },
     { path: '/login', name: 'login', component: Login },
     { path: '/register', name: 'register', component: Register },
     { path: '/events', name: 'events', component: Events },
-    { path: '/:pathMatch(.*)*', redirect: '/' }, // Redirect to home for any unmatched routes
+    { path: '/refresh', name: 'refresh', component: Refresh },
+    { path: '/:pathMatch(.*)*', redirect: '/' },
 ];
 
 const router = createRouter({
