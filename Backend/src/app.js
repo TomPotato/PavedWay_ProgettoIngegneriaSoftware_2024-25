9a80c4adfcf63674a165ad3bb36681a592acaa77
Valerio Cassone <valerio.cassone@studenti.unitn.it>
2025-05-27 10:45:24 +0200

    Typo in Cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 app.use(cors(
     {
         origin: 'http://localhost:5173',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        methods: ['GET', 'POST', 'PATCH', 'DELETE'],
         credentials: true,
     }
 ));
