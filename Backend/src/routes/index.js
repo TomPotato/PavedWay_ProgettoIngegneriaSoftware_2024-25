9736a9df0dc333cdd35df4460ca02cf5087fdadb
tommaso <tommaso.agosti09@gmail.com>
2025-06-01 16:51:40 +0200

    Minor changes to index, added new paths

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -5,14 +5,14 @@ const authRoutes = require('./authRoutes');
 const userRoutes = require('./userRoutes');
 const reportRoutes = require('./reportRoutes');
 const siteRoutes = require('./siteRoutes');
-
+const pathRoutes = require('./pathRoutes');
 const geocodeRoutes = require('./geocodeRoutes');
 
 router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
 router.use('/reports', reportRoutes);
 router.use('/sites', siteRoutes);
-
+router.use('/paths', pathRoutes);
 router.use('/geocode', geocodeRoutes);
 
 module.exports = router;
