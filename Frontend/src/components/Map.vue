cc56564dfc6a66b20fed50cf9ec47cbd157eb0fe
Valerio Cassone <valerio.cassone@studenti.unitn.it>
2025-06-09 16:33:58 +0200

    Fix for Leaflet icons

--- Frontend/src/components/Map.vue
+++ Frontend/src/components/Map.vue
@@ -79,6 +79,18 @@ import { ref, onMounted, watch } from 'vue';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: markerIcon2x,
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow,
+});
+
 import siteService from '@/services/SiteService';
 import reportService from '@/services/ReportService';
 import MapService from '@/services/MapService';
